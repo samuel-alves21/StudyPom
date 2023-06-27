44990f704a1b7c0b582715ddf7a6899b1e46a7b8
Samuel Alves <samueltheodoro21@gmail.com>
2023-06-26 22:47:40 -0300

    color picker added

--- src/components/UserConfig/SetConfig/ConfigWindow/SetColor/index.tsx
+++ src/components/UserConfig/SetConfig/ConfigWindow/SetColor/index.tsx
@@ -1,3 +1,29 @@
+import { ChromePicker } from 'react-color'
+import { useContext } from 'react'
+import { CustomizationContext, CustomizationContextType } from '../../../../../contexts/CustomizationContext'
+import styled from 'styled-components'
+
 export const SetColor = () => {
-  return <h1>setColor</h1>
+  const { customizationDispatch, customizationState: { color } } = useContext(CustomizationContext) as CustomizationContextType
+
+  return (
+    <Wrapper>
+      <ChromePicker disableAlpha={true} color={color} onChange={ (color) => customizationDispatch({ type: 'CHANGE_COLOR', payload: color.hex }) }/>
+    </Wrapper>
+  )
 }
+
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  & .chrome-picker {
+    font-family: 'Roboto', sans-serif !important;
+    border-radius: 5px !important;
+  }
+
+  & .chrome-picker > div {
+    border-radius: 5px 5px 0 0 !important;
+  }
+`
