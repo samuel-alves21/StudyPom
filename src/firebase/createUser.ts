e80235fc45ee1bb5d8df771a21aaf4027fbb0db4
Samuel Alves <samueltheodoro21@gmail.com>
2023-09-03 04:11:16 -0300

    adjusting page redirects

---
+++ src/firebase/createUser.ts
@@ -0,0 +1,21 @@
+import { createUserWithEmailAndPassword, updateProfile } from "firebase/auth"
+import { auth, database } from "./config"
+import { push, ref, set } from "firebase/database"
+
+export const createUser = async (email: string, password: string, username: string) => {
+  const userCredential = await createUserWithEmailAndPassword(auth, email, password)
+
+  await updateProfile(userCredential.user, {
+    displayName: username
+  })
+
+  await set(ref(database, 'users/' + userCredential.user.uid), {
+    username: username,
+    email: email,
+    id: userCredential.user.uid,
+  })
+
+  await push(ref(database, 'username/'), {
+    username: username,
+  })
+}
