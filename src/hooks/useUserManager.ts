ee4e7c9ce7510206676ec4a8cf3a79e2df408bab
Samuel Alves <samueltheodoro21@gmail.com>
2023-09-17 13:55:59 -0300

    creating the user config on database

--- src/hooks/useUserManager.ts
+++ src/hooks/useUserManager.ts
@@ -3,12 +3,16 @@ import { useContext, useEffect } from 'react'
 import { auth, database } from '../firebase/config'
 import { ref, remove } from 'firebase/database'
 import { UserContext, UserContextType } from '../contexts/UserContext'
+import { TimerContext, TimerContextType } from '../contexts/TimerContext'
+import { getUserConfig } from '../firebase/getUserConfig'
 
 export const useUserManager = (setIsLoading: (value: boolean) => void) => {
   const {
     userState: { pendentUser },
     userDispatch,
   } = useContext(UserContext) as UserContextType
+
+  const { timeDispatch } = useContext(TimerContext) as TimerContextType
 
   useEffect(() => {
     if (pendentUser) {
@@ -21,7 +25,12 @@ export const useUserManager = (setIsLoading: (value: boolean) => void) => {
         if (!firebaseUser.emailVerified) {
           window.location.replace('/StudyPom/emailVerification/login')
         } else {
-          await remove(ref(database, 'users/' + firebaseUser.uid + '/register'))
+          try {
+            await remove(ref(database, 'users/' + firebaseUser.uid + '/register'))
+            await getUserConfig(firebaseUser, timeDispatch)
+          } catch (error) {
+            console.error(error)
+          }
           userDispatch({ type: 'SET_EMAIL', payload: firebaseUser.email as string })
           userDispatch({ type: 'SET_ID', payload: firebaseUser.uid })
           userDispatch({ type: 'SET_USERNAME', payload: firebaseUser.displayName as string })
@@ -37,5 +46,5 @@ export const useUserManager = (setIsLoading: (value: boolean) => void) => {
       unsubscribe()
       window.location.replace('/StudyPom/register')
     }
-  }, [pendentUser, setIsLoading, userDispatch])
+  }, [pendentUser, setIsLoading, userDispatch, timeDispatch])
 }
