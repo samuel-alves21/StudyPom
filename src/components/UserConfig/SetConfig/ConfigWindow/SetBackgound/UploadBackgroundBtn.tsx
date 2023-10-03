2f69ac83b11a9d13351b217919d34cdda6a70e80
Samuel Alves <110740160+samuel-alves21@users.noreply.github.com>
2023-10-03 06:50:22 -0300

    styling the upload btn

--- src/components/UserConfig/SetConfig/ConfigWindow/SetBackgound/UploadBackgroundBtn.tsx
+++ src/components/UserConfig/SetConfig/ConfigWindow/SetBackgound/UploadBackgroundBtn.tsx
@@ -1,15 +1,34 @@
+import { useState } from 'react'
 import styled from 'styled-components'
 
 export const UploadBackgroundBtn = () => {
-  return <Button className='form-button'>upload background</Button>
+  const [image, setImage] = useState<File>()
+
+  const handleImageUpload = (e: React.ChangeEvent<HTMLInputElement>) => {
+    if (!e.target.files) return
+    setImage(e.target.files[0])
+  }
+
+  return (
+    <>
+      <Button className='form-button' as='label' htmlFor="file-upload-btn">
+        {image?.name || 'upload background'}
+      </Button>
+      <input type="file" accept="image/*" style={{ display: 'none' }} id="file-upload-btn" onChange={(e) => handleImageUpload(e)} />
+    </>
+  )
 }
 
 const Button = styled.button`
   width: 180px;
   margin: 20px auto 0px;
+  color: #fff;
+  text-align: center;
+  background-color: var(--color-primary);
 
   @media (hover: hover) and (pointer: fine) {
     &:hover {
+      opacity: 0.8;
       color: #fff;
     }
   }
