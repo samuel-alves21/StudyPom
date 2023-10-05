3b5c4f3c187fd308b06550cbe2f0ca9655c77bd9
Samuel Alves <110740160+samuel-alves21@users.noreply.github.com>
2023-10-05 12:45:47 -0300

    saving the background config

---
+++ src/firebase/getDefaultBackground.ts
@@ -0,0 +1,7 @@
+import { getDownloadURL, ref } from 'firebase/storage'
+import { storage } from './config'
+
+export const getDefaultBackground = async () => {
+  const url = await getDownloadURL(ref(storage, 'background/default-bg.jpg'))
+  return url
+}
