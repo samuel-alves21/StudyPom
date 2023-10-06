f478c8e4c0c18658b38dd5767dc1db7aa46df1c5
Samuel Alves <110740160+samuel-alves21@users.noreply.github.com>
2023-10-06 18:32:29 -0300

    removing unnecessary code

--- src/firebase/setUserLoggedWithGoogle.ts
+++ src/firebase/setUserLoggedWithGoogle.ts
@@ -1,22 +1,14 @@
-import { UserCredential, updateProfile } from 'firebase/auth'
-import { get, ref, set } from 'firebase/database'
+import { UserCredential } from 'firebase/auth'
+import { ref, set } from 'firebase/database'
 import { database } from './config'
 
 export const setUserLoggedWithGoogle = async (result: UserCredential | null) => {
   if (result) {
-    const snapshot = await get(ref(database, `users/${result.user.uid}`))
-    if (snapshot.exists()) {
-      await set(ref(database, 'users/' + result.user.uid), {
-        username: snapshot.val().username,
-        email: result.user.email,
-        id: result.user.uid,
-      })
-    } else {
-      await set(ref(database, 'users/' + result.user.uid), {
-        username: result.user.displayName,
-        email: result.user.email,
-        id: result.user.uid,
-      })
-    }
+    await set(ref(database, 'users/' + result.user.uid), {
+      username: result.user.displayName,
+      email: result.user.email,
+      id: result.user.uid,
+    })
   }
 }
+
